f5a3d2a62e0c7b286ce60b9540098214651485bd
sheldonlai <sheldonlai@hotmail.com>
2018-05-24 10:33:18 -0700

    - updated about page

--- client/about/About.tsx
+++ client/about/About.tsx
@@ -2,9 +2,10 @@ import * as React from 'react';
 import Grid from "@material-ui/core/Grid/Grid";
 import Typography from "@material-ui/core/Typography/Typography";
 import {CSSTransition} from "react-transition-group";
-
+import {work_experiences} from "../contents/about_content";
 
 const margin = {marginTop: 10};
+
 export class About extends React.Component {
 
   state = {
@@ -17,26 +18,29 @@ export class About extends React.Component {
 
   render() {
     return (
-      <CSSTransition
-        in={!this.state.loading}
-        timeout={800}
-        classNames="fadeslow">
-        {() => (
-          <Grid container justify="center">
-            <Grid item sm={12} md={8} lg={6}>
+      <Grid container justify="center">
+        <CSSTransition
+          in={!this.state.loading}
+          timeout={800}
+          classNames="fadeslow">
+          {() => (
+
+            <Grid item xs={12} sm={12} md={8} lg={6}>
               <Typography variant="display1" style={{textAlign: "center", ...margin}}>About Me</Typography>
-              <Typography variant="body2" style={margin}>
+              <Typography variant="body2" style={margin} gutterBottom>
                 I am a soon to be graduated Computer Science student from UBC. I have a strong interest in software
                 engineering. In other words, I just like solving moderately hard problems efficiently. With that said,
                 I am still pretty good at mundane tasks like implementing and designing websites. In the field of
                 machine learning, I am no expert, but if you give me the title to a published paper I could probably
-                write the implementation and apply it. Recently, I have grown quite fond of experimenting with ML projects.
+                write the implementation and apply it. Recently, I have grown quite fond of experimenting with ML
+                projects.
               </Typography>
 
-              <Typography variant="body2" style={margin}>
+              <Typography variant="headline" gutterBottom>Past Work Experiences</Typography>
+              {work_experiences.map((e, i) => e.toDOM(i))}
+              <Typography variant="body2" gutterBottom style={margin}>
                 If you have any questions or you wish to hire me please contact with the email below:
               </Typography>
-
               <Typography variant="headline" style={margin}>
                 Contact Me @:
               </Typography>
@@ -44,9 +48,10 @@ export class About extends React.Component {
                 sheldonlai@hotmail.com
               </Typography>
             </Grid>
-          </Grid>
-        )}
-      </CSSTransition>
+
+          )}
+        </CSSTransition>
+      </Grid>
     )
   }
 }
---
+++ client/contents/about_content.ts
@@ -0,0 +1,18 @@
+
+import {WorkExperience} from "../about/WorkExperience";
+
+export const work_experiences = [
+    new WorkExperience({
+        company: "Xtract AI",
+        position: "Software Developer Intern",
+        startDate: "May 2017",
+        endDate: "Sep 2017"
+    }),
+    new WorkExperience({
+        company: "Parahon Testing Enterprises",
+        position: "Software Developer Intern",
+        startDate: "Sep 2015",
+        endDate: "May 2016"
+    }),
+
+];
